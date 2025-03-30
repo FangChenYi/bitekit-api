0f9af14d02b2aaf98c9ef9e84bad05f12e553f15
FangChenYi <fx1.github@gmail.com>
2025-03-30 13:05:50 +0800

    init

--- src/main/java/com/bitekit/bitekit/api/service/login/ILoginService.java
+++
@@ -1,8 +0,0 @@
-package com.bitekit.bitekit.api.service.login;
-
-import com.bitekit.bitekit.api.dto.response.LoginResponse;
-import com.bitekit.bitekit.api.dto.request.LoginRequest;
-
-public interface ILoginService {
-    LoginResponse login(LoginRequest request);
-}
--- src/main/java/com/bitekit/bitekit/api/service/user/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.bitekit.bitekit.api.service.user;
-
-import com.bitekit.bitekit.api.dto.request.RegisterRequest;
-import com.bitekit.bitekit.api.dto.response.RegisterResponse;
-
-public interface IUserService {
-    RegisterResponse register(RegisterRequest request);
-}
